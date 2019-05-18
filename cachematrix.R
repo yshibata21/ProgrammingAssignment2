596919224d240f6f66a41bcf73c34a83721a10bd
Yuichi Shibata <wesensform@icloud.com>
2019-05-18 12:27:49 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
